ae18abfaba075650cacfbd3ad8a6f4dfb30cd0d3
Janeth Eni <enijaneth@gmail.com>
2023-04-01 03:48:28 +0100

    String concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated concatination function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the resulting string.
+    """
+    return str1 + str2
