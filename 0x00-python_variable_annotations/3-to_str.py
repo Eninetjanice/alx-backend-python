2dd857d16ad4f0bf9a35621383e252bb49273842
Janeth Eni <enijaneth@gmail.com>
2023-04-01 04:01:57 +0100

    Basic annotation to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Basic annotation to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This takes a float n & returns the string representation of the float
+    """
+    return str(n)
