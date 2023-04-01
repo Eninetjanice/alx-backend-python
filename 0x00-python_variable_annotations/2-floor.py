db2f1e1b7780ed5f3989d9ef2214f0a34f5b0bf7
Janeth Eni <enijaneth@gmail.com>
2023-04-01 03:56:15 +0100

    Foor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function floor
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that takes a float n as argument & returns the floor of the float
+    """
+    return math.floor(n)
