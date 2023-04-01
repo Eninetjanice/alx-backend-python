50086a924826d4976135a42a09104598594a0ce3
Janeth Eni <enijaneth@gmail.com>
2023-04-01 05:53:22 +0100

    string and int/float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Func to_kv that takes a str k & an int OR float v as args and returns a tuple.
+The first element of the tuple is the string k.
+The second element is the square of the int/float v & annotated as a float.
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple containing a string k and the square of an int/float v as a float.
+    """
+    return (k, v ** 2)
