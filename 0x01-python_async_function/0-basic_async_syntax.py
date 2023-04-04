51b958358304345482e696f64b9c5a271b3002ab
Janeth Eni <enijaneth@gmail.com>
2023-04-04 14:36:10 +0100

    Checks

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -14,11 +14,3 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-
-async def main():
-    """Asynchronous coroutine that runs wait_random() and prints the result"""
-    delay = await wait_random()
-
-if __name__ == "__main__":
-    asyncio.run(main())
