f1e16f6cfc021dbbd8f50a2f9d231e9bf46fe77e
Janeth Eni <enijaneth@gmail.com>
2023-04-05 05:33:46 +0100

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+A coroutine called async_comprehension that takes no arguments.
+'''
+
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """
+    Asynchronous comprehension that collects 10 random ints
+    and returns them as a list.
+    """
+    return [num async for num in async_generator()]
