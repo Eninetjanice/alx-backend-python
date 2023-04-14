17ed64a9efed8d2110e95da73ca8bcaf0c744e55
Janeth Eni <enijaneth@gmail.com>
2023-04-14 07:27:38 +0100

    Correct annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -15,8 +15,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     return zoomed_in
 
 
-array: Tuple[int, int, int] = (12, 72, 91)
+array: Tuple = (12, 72, 91)
 
-zoom_2x: Tuple[int, ...] = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: Tuple[int, ...] = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)
